d78660b5b799a2fc18da80828347538414265b45
Mireia PORTA <mporta@hamelinbrands.com>
2026-06-16 13:08:38 +0200

    Treta part directori cylinders perquè funcioni bé a tot arreu

--- public/layout_operari.php
+++ public/layout_operari.php
@@ -7,7 +7,7 @@ function renderOperariPage($title, $subtitle, $content) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= htmlspecialchars($title) ?></title>
-  <link rel="stylesheet" href="/Cylinders/public/assets/css/tailwind.min.css">
+  <link rel="stylesheet" href="assets/css/tailwind.min.css">
 </head>
 <body class="bg-gray-100">
 
